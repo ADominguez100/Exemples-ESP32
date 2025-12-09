918255424c507a8a2724251c53154f72c47ecf69
ADominguez <adomingue3@inscamidemar.cat>
2025-12-09 20:30:54 +0100

    connectivity c file modified: HTTP method used now

--- connectivity.c
+++ connectivity.c
@@ -1,72 +1,60 @@
-// Learn about the ESP32 WiFi simulation in
-// https://docs.wokwi.com/guides/esp32-wifi
-
 #include <WiFi.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <HTTPClient.h>
 
-LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
 
-#define NTP_SERVER     "pool.ntp.org"
-#define UTC_OFFSET     0
-#define UTC_OFFSET_DST 0
-
-void spinner() {
-  static int8_t counter = 0;
-  const char* glyphs = "\xa1\xa5\xdb";
-  LCD.setCursor(15, 1);
-  LCD.print(glyphs[counter++]);
-  if (counter == strlen(glyphs)) {
-    counter = 0;
-  }
-}
-
-void printLocalTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    LCD.setCursor(0, 1);
-    LCD.println("Connection Err");
-    return;
-  }
-
-  LCD.setCursor(8, 0);
-  LCD.println(&timeinfo, "%H:%M:%S");
-
-  LCD.setCursor(0, 1);
-  LCD.println(&timeinfo, "%d/%m/%Y   %Z");
-}
+const String url = "https://www.positive-api.online/phrase/esp";
 
 void setup() {
   Serial.begin(115200);
+  delay(1000); // Pequeña pausa para estabilizar el serial
+  
+  Serial.println("\n=== INICIO DEL PROGRAMA ===");
+  
+  WiFi.begin(ssid, password);
 
-  LCD.init();
-  LCD.backlight();
-  LCD.setCursor(0, 0);
-  LCD.print("Connecting to ");
-  LCD.setCursor(0, 1);
-  LCD.print("WiFi ");
-
-  WiFi.begin("Wokwi-GUEST", "", 6);
+  Serial.print("Connectant-se al WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    spinner();
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
+  Serial.println("\n✓ WiFi connectat correctament!");
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+  Serial.println("----------------------------");
 
-  LCD.clear();
-  LCD.setCursor(0, 0);
-  LCD.println("Online");
-  LCD.setCursor(0, 1);
-  LCD.println("Updating time...");
+  Serial.println("Obtenint resposta de la API...");
 
-  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+  HTTPClient http;
+  http.begin(url);
+
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode > 0) {
+    Serial.println("----------------------------");
+    Serial.print("✓ HTTP Response Code: ");
+    Serial.println(httpResponseCode);
+    Serial.println("----------------------------");
+    
+    String payload = http.getString();
+    Serial.println("RESPOSTA DE L'API:");
+    Serial.println(payload);
+    Serial.println("----------------------------");
+  }
+  else {
+    Serial.println("----------------------------");
+    Serial.print("✗ Error code: ");
+    Serial.println(httpResponseCode);
+    Serial.println("No s'ha pogut obtenir la resposta :-(");
+    Serial.println("----------------------------");
+  }
+  
+  http.end(); // Liberar recursos
+  Serial.println("\n=== PROGRAMA FINALITZAT ===");
 }
 
 void loop() {
-  printLocalTime();
-  delay(250);
+  delay(100);
 }
